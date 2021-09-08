0e988086cf015115e5e99b5003ec7a431fb4d6b7
Kate <katefa46@gmail.com>
2021-09-08 17:29:15 -0400

    Created API request format for adding images

--- business-logic.js
+++ business-logic.js
@@ -1,24 +1,27 @@
 //Kate Allsebrook
 
-function addImages(name,urls) {
-
+function addImages(imageArray) {
+    console.log(imageArray)
     /*
-    base('Images').create({
-        "Name": "Coffee plant",
-        "Attachment": [
-          {
-            "url": "https://dl.airtable.com/.attachments/4417e4a618535331e1e25021f2ac3181/f1287f02/Coffee-Plant-.jpg"
+    base('Images').create([
+        {
+          "fields": {
+            "Name": "Coffee plant",
+            "Image": [
+              {
+                "url": "https://dl.airtable.com/.attachments/4417e4a618535331e1e25021f2ac3181/f1287f02/Coffee-Plant-.jpg"
+              }
+            ]
           }
-        ]
-      }, function(err, record) {
+        }],
+         function(err, records) {
         if (err) {
           console.error(err);
           return;
         }
-        console.log(record.getId());
       });
     */
 }
 
 //Export
-module.exports = {};
+module.exports = {addImages};
--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const express = require('express');
 let Airtable = require('airtable');
 const bodyParser = require('body-parser');
-
-//const model = require('./business-logic.js');
+const model = require('./business-logic.js');
 
 //Express setup + middleware
 let app = express();
@@ -17,10 +16,11 @@ Airtable.configure({
 const base = Airtable.base('app1lDw2Y6fFh8UXi');
 
 //Routes
-app.route("/add") //Must use x-www-form-urlencoded if testing in Postman
+app.route("/add")
 	.post((req,res) => {
+		model.addImages(req.body["Images"])
 		res.status(200)
-		res.send(req.body)
+		res.send("Thanks!")
 	})
 
 //Param Example
